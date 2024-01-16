95e209053766cf8ef29ee032b8969af0a8ccfe2a
Sayyed Asad Ullah <developerfrontend786@gmail.com>
2024-01-16 21:11:01 +0500

    sau_totaliser

--- src/app.js
+++ src/app.js
@@ -1,31 +1,55 @@
+import { useState } from "react";
+import Outputs from "./Outputs";
+
 export default function App() {
-    return <div>
-        <div>
-            <input type="checkbox" className="theme" />
-            <h1 className="result">0</h1>
-            <p>0</p>
-        </div>
-        <div className="inputs">
-            <button>AC</button>
-            <button>DEL</button>
-            <button>%</button>
-            <button>^</button>
-            <button>/</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>-</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
-        </div>
+    const [exp, setExp] = useState('');
+    const [res, setRes] = useState('0');
+    return <div className="calc">
+        <Outputs exp={exp} res={res} />
+        <Inputs setExp={setExp} setRes={setRes} exp={exp} res={res} />
+
+    </div>
+};
+
+function Inputs({ setExp, setRes, res, exp }) {
+    function expHadler(e) {
+        const click = e.target.textContent;
+        if (click === 'AC') {
+            setExp('');
+            setRes('0');
+        }
+        else if (click === 'DEL') {
+            setExp(exp => exp.slice(0, -1));
+        }
+        else if (click === '=') {
+            setExp(exp => exp.replaceAll('^', '**'));
+
+        }
+
+        else {
+            setExp(exp => exp += click);
+        }
+    }
+    return <div className="inputs" onClick={expHadler}>
+        <button>AC</button>
+        <button>DEL</button>
+        <button>%</button>
+        <button>^</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>/</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>*</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+        <button>-</button>
     </div>
 }
