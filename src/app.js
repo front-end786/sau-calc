c62a4d851a5890f90ae5b97c69f2043c039c709b
Sayyed Asad Ullah <developerfrontend786@gmail.com>
2024-01-16 16:55:22 +0500

    sau_changes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 export default function App() {
-    <div>HELLO SAU</div>
+    return <div>
+        <div>
+            <input type="checkbox" className="theme" />
+            <h1 className="result">0</h1>
+            <p>0</p>
+        </div>
+        <div className="inputs">
+            <button>AC</button>
+            <button>DEL</button>
+            <button>%</button>
+            <button>^</button>
+            <button>/</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>*</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </div>
+    </div>
 }
